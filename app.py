8e6c40838da24bdc3e0d9959be1645457cee5984
Hyliufl <hyliu0102@gmail.com>
2025-08-08 12:26:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,11 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score, confusion_matrix
-
-
 from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_excel('Disease_symptom_and_patient_profile_dataset.xlsx', engine='openpyxl')
-st.title("diagnose-AI")
+df = pd.read_csv('Disease_symptom_and_patient_profile_dataset.csv')
 
+# DATA PROCESSING AND PIPELINE
 features = ['Fever', 'Cough', 'Difficulty Breathing', 'Age', 'Gender', 'Blood Pressure', 'Cholesterol Level']
 target = 'Disease'
 X = df[features]
@@ -34,42 +32,45 @@ pipeline = Pipeline(steps=[
 ])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 pipeline.fit(X_train, y_train)
-user_input = {}
-print(df)
-for col in features:
-    options = df[col].dropna().unique()
-    print(f"{col} options: {', '.join(map(str, options))}")
-    #user_input[col] = st.selectbox(f"Select {col}:", options=list(options))
-user_df = pd.DataFrame([user_input])
-print(user_df)
-print(X_test)
-predicted_disease = pipeline.predict(user_df)[0]
-print(f"\n:stethoscope: Based on your symptoms, the predicted disease is: **{predicted_disease}**")
 
 
 
-import google.generativeai as genai
-import pandas as pd
+# STREAMLIT
+st.title("Health Input Recorder")
 
-genai.configure(api_key.secret("AI_key"))
-model = genai.GenerativeModel("gemini-2.5-flash")
+fields = {
+    "Age": st.number_input("Age", min_value=0, max_value=120, step=1),
+    "Cholesterol Level": st.selectbox("Cholesterol Level", ["Low", "Normal", "High"]),
+    "Blood Pressure": st.selectbox("Blood Pressure", ["Low", "Normal", "High"]),
+    "Gender": st.selectbox("Gender", ["Male", "Female"]),
+    "Difficulty Breathing": st.radio("Difficulty Breathing", ["Yes", "No"]),
+    "Fever": st.radio("Fever", ["Yes", "No"]),
+    "Cough": st.radio("Cough", ["Yes", "No"])
+}
 
-#symptoms = input("Enter a describtion of your symptoms:")
+description = st.text_area("description")
 
-data = pd.read_csv('medquad.csv', on_bad_lines='skip')
-st.html("<hr>")
-Arjun= st.text_input("Enter a description of your symptoms:", key="symptoms")
+if st.button("Record Inputs"):
+    user_inputs = fields
+    user_df = pd.DataFrame([user_inputs])
+    predicted_disease = pipeline.predict(user_df)[0]
+    st.success(f"Based on your symptoms, the predicted disease is: **{predicted_disease}**")
 
 
-prompt = f"""
-In a professional but also easy to understand voice, give a list of possible diseases or conditions that these symptoms
-indicate and with the help of the this data: {data}. Do not refer to "data" or "dataset".
-Here are the results from a classification model based on patient profiles and symptoms: {predicted_disease}
-List the items by starting with the one that is most relevant to the user and the most common. For each possibility, briefly explain why it may be relevant, how common it is
-and what you need to do if you have it.These are the symptom{ Arjun }
-"""
+#LLM
+    import google.generativeai as genai
+    genai.configure(api_key="AIzaSyDJO-54h79z5dEgoIHiOPANDIJMVHZppNs")
+    model = genai.GenerativeModel("gemini-2.5-flash")
+    data = pd.read_csv('medquad.csv', on_bad_lines='skip')
 
-text1 = model.generate_content([prompt])
+    prompt = f"""
+    In a professional but also easy to understand voice, give a list of possible diseases or conditions that these symptoms
+    indicate and with the help of the this data: {data}. Do not refer to "data" or "dataset".
+    Here are the results from a classification model based on patient profiles and symptoms: {predicted_disease}
+    List the items by starting with the one that is most relevant to the user and the most common. For each possibility, briefly explain why it may be relevant, how common it is
+    and what you need to do if you have it.
+    These are the symptoms: {user_inputs}. The patient also provided this description: {description}.
+    """
 
-print(text1.text)
-
+    text1 = model.generate_content([prompt])
+    st.write(text1.text)
